179229a7df574d979c5f53769406e705e39083a8
nguyen bao <ng.tanquocbao@gmail.com>
2024-11-04 02:53:13 +0000

    move to py 3.13

--- function.py
+++ function.py
@@ -1,12 +1,14 @@
-from bao import *
-from vnstock3 import Vnstock as vn
-import dask.dataframe as dd
+import pytz
 import shutil
 import os
+import time
 import pyarrow as pa
 import pyarrow.parquet as pq
-import time
-import pytz
+import plotly.express as px
+import plotly.graph_objects as go
+from bao import *
+from vnstock3 import Vnstock as vn
+
 
 def get_ticker(path, dictionary) -> pd.DataFrame:
     """_summary_
@@ -26,6 +28,97 @@ def get_ticker(path, dictionary) -> pd.DataFrame:
     return data
 
 
+def draw_SMA(data, col, windows) -> None:
+    """_summary_
+    give visual for simple moving average with given list of windows
+    Args:
+        data (_type_): dataframe
+        col (_type_): value column
+        windows (_type_): list of window period
+    """
+
+    for i in windows:
+        data[col + str(i)] = data[col].rolling(window=i).mean()
+    fig = go.Figure()
+    for i in windows:
+        fig.add_trace(
+            go.Scatter(
+                x=data["time"],  # Adjust if your index is not time
+                y=data[col + str(i)],
+                mode="lines",
+                name=str(i),
+            )
+        )
+
+        # Update layout
+        fig.update_layout(
+            title="Simple moving average return",
+            xaxis_title="Time",
+            yaxis_title="Rolling Mean Return",
+            showlegend=True,
+        )
+    fig.show()
+
+
+def draw_cummulative(data, col, windows) -> None:
+    """_summary_
+    give visual for simple moving average with given list of windows
+    Args:
+        data (_type_): dataframe
+        col (_type_): value column
+        windows (_type_): list of window period
+    """
+
+    for i in windows:
+        data[col + str(i)] = data[col].rolling(window=i).sum()
+    fig = go.Figure()
+    for i in windows:
+        fig.add_trace(
+            go.Scatter(
+                x=data["time"],  # Adjust if your index is not time
+                y=data[col + str(i)],
+                mode="lines",
+                name=str(i),
+            )
+        )
+
+        # Update layout
+        fig.update_layout(
+            title="Cumulative return rolling back",
+            xaxis_title="Time",
+            yaxis_title="Rolling Mean Return",
+            showlegend=True,
+        )
+    fig.show()
+
+
+def draw_EMA(data, col, windows) -> None:
+    """_summary_
+    Give visualization for Exponential Moving Average
+    Args:
+        data (_type_): dataframe
+        col (_type_): value column
+        windows (_type_): list of window period
+    """
+    for i in windows:
+        data[col + str(i)] = data[col].ewm(span=i, adjust=True).mean()
+    fig = go.Figure()
+    for i in windows:
+        fig.add_trace(
+            go.Scatter(
+                x=data["time"],  # Adjust if your index is not time
+                y=data[col + str(i)],
+                mode="lines",
+                name=str(i),
+            )
+        )
+        # Update layout
+        fig.update_layout(
+            title="Exponential Moving Average Return", showlegend=True
+        )
+    fig.show()
+
+
 def update_ticker(ticker, path, dictionary) -> pd.DataFrame:
     """_summary_
     update and add removed flag if there is missing data
@@ -39,26 +132,34 @@ def update_ticker(ticker, path, dictionary) -> pd.DataFrame:
     stock = vn(show_log=False).stock(symbol="ABC", source="VCI")
     new_ticker = stock.listing.symbols_by_exchange()
     ticker.loc[
-        ~ticker[dictionary["ticker"]].isin(new_ticker[dictionary["ticker"]]),
+        ~ticker[dictionary["ticker"]].isin(
+            new_ticker[dictionary["ticker"]]
+        ),
         "dropped",
     ] = datetime.today()
-    new_ticker = new_ticker[~new_ticker[dictionary["ticker"]].
-                            isin(ticker[dictionary["ticker"]])]
+    new_ticker = new_ticker[
+        ~new_ticker[dictionary["ticker"]].isin(
+            ticker[dictionary["ticker"]]
+        )
+    ]
     if new_ticker.shape[0] > 0:
         for i in new_ticker[dictionary["ticker"]]:
             try:
                 new_ticker.loc[
                     new_ticker[dictionary["ticker"]] == i,
                     "total_outstanding",
-                ] = stock.trading.price_board(
-                    [i])[dictionary["share_outstanding"][0]][
-                        dictionary["share_outstanding"][1]][0]
+                ] = stock.trading.price_board([i])[
+                    dictionary["share_outstanding"][0]
+                ][
+                    dictionary["share_outstanding"][1]
+                ][
+                    0
+                ]
             except Exception as e:
                 print(i, e)
         ticker = pd.concat([ticker, new_ticker])
         print("updated")
-        shutil.rmtree(path)
-        print(f"{path} has been removed.")
+        clean_backup_data(path, clean=True)
         ticker.to_parquet(path, index=False)
     return ticker
 
@@ -76,11 +177,16 @@ def get_full_ticker(path, dictionary) -> None:
             new_ticker.loc[
                 new_ticker[dictionary["ticker"]] == i,
                 "total_outstanding",
-            ] = stock.trading.price_board(
-                [i])[dictionary["share_outstanding"][0]][
-                    dictionary["share_outstanding"][1]][0]
+            ] = stock.trading.price_board([i])[
+                dictionary["share_outstanding"][0]
+            ][
+                dictionary["share_outstanding"][1]
+            ][
+                0
+            ]
             time.sleep(
-                1)  # api might not respond when the request speed is too high
+                1
+            )  # api might not respond when the request speed is too high
 
         except Exception as e:
             print(i, e)
@@ -90,12 +196,14 @@ def get_full_ticker(path, dictionary) -> None:
         print(f"{path} has been removed.")
     else:
         print(f"{path} does not exist.")
-    new_ticker.to_parquet(path,
-                          index=False,
-                          partition_cols=[dictionary["exchange"]])
+    new_ticker.to_parquet(
+        path, index=False, partition_cols=[dictionary["exchange"]]
+    )
 
 
-def read_1_ticker(ticker, start_date, end_date, dictionary) -> pd.DataFrame:
+def read_1_ticker(
+    ticker, start_date, end_date, dictionary
+) -> pd.DataFrame:
     """_summary_
     read data daily for 1 ticker
     Args:
@@ -107,15 +215,31 @@ def read_1_ticker(ticker, start_date, end_date, dictionary) -> pd.DataFrame:
     Returns:
         _type_: _description_
     """
-    return  (vn(show_log=False).stock(
-        symbol=ticker,
-        source=dictionary["source"]).quote.history(start=start_date,
-                                                    end=end_date,
-                                                    interval="1D"))
-def read_1_ticker_intra(ticker,dictionary):
-    return (vn(show_log=False).stock(
-        symbol=ticker,
-        source=dictionary['source']).quote.intraday(symbol=ticker,page_size=10000000, show_log=False))
+    return (
+        vn(show_log=False)
+        .stock(symbol=ticker, source=dictionary["source"])
+        .quote.history(start=start_date, end=end_date, interval="1D")
+    )
+
+
+def read_1_ticker_intra(ticker, dictionary) -> None:
+    """_summary_
+    Read and save intradata daily
+    Args:
+        ticker (_type_): ticker list
+        dictionary (_type_): column name
+
+    Returns:
+        _type_: None
+    """
+    return (
+        vn(show_log=False)
+        .stock(symbol=ticker, source=dictionary["source"])
+        .quote.intraday(
+            symbol=ticker, page_size=10000000, show_log=False
+        )
+    )
+
 
 def get_past_Friday() -> datetime.date:
     """_summary_
@@ -123,7 +247,7 @@ def get_past_Friday() -> datetime.date:
     Returns:
         _type_: _description_
     """
-    date0 = datetime.today().date()-pd.offsets.DateOffset(1)
+    date0 = datetime.today().date() - pd.offsets.DateOffset(1)
     while date0.weekday() != 4:  # 5 = Saturday, 4 = Friday
         date0 -= pd.offsets.DateOffset(1)
     return date0.strftime("%Y-%m-%d")
@@ -140,21 +264,45 @@ def get_full_data(path_ticker, path_out, dictionary):
     fal_tick = []
     tickers = get_ticker(path_ticker, dictionary)
     data = pd.DataFrame()
-    for tick, ex in zip(tickers[dictionary["ticker"]],
-                        tickers[dictionary["exchange"]]):
+    for tick, ex in zip(
+        tickers[dictionary["ticker"]], tickers[dictionary["exchange"]]
+    ):
         try:
-            temp = read_1_ticker(tick, "2013-01-01", get_past_Friday(),
-                                dictionary)
+            temp = read_1_ticker(
+                tick, "2013-01-01", get_past_Friday(), dictionary
+            )
             temp["ticker"] = tick
             temp["exchange"] = ex
             data = pd.concat([data, temp], axis=0)
         except Exception as e:
             fal_tick.append(tick)
-    if os.path.exists(path_out):
-        shutil.rmtree(path_out)
-        print(f"{path_out} has been removed.")
+    clean_backup_data(path_out)
     table = pa.Table.from_pandas(data)
     pq.write_to_dataset(table, path_out)
+    print("sucessful create full data")
+
+
+def clean_backup_data(path, clean=False) -> None:
+    if os.path.exists(path):
+        # Remove the directory or file
+        source_dir = os.path.dirname(path)
+
+        # Create the backup directory within the same structure
+        backup_dir = os.path.join(source_dir, "backup")
+        if not os.path.exists(backup_dir):
+            os.makedirs(backup_dir)
+
+        # Get the filename of the source file
+        filename = os.path.basename(path)
+
+        # Construct the full path for the backup file
+        backup_path = os.path.join(backup_dir, filename)
+
+        # Copy the source file to the backup location
+        shutil.copy2(path, backup_path)
+        if clean:
+            shutil.rmtree(path)
+            print(f"{path} has been removed.")
 
 
 def get_data(path_out, path_ticker, dictionary) -> pd.DataFrame:
@@ -181,7 +329,9 @@ def get_data(path_out, path_ticker, dictionary) -> pd.DataFrame:
     latest_friday = get_past_Friday()
     tickers = get_ticker(path_ticker, dictionary)
     if lastest_data_date.strftime("%Y-%m-%d") < latest_friday:
-        lastest_data_date = lastest_data_date + pd.offsets.DateOffset(1)
+        lastest_data_date = lastest_data_date + pd.offsets.DateOffset(
+            1
+        )
         data = pd.DataFrame()
         fal_tick = []
         for tick, ex in zip(tickers.symbol, tickers.exchange):
@@ -196,27 +346,50 @@ def get_data(path_out, path_ticker, dictionary) -> pd.DataFrame:
                 data = pd.concat([data, temp], axis=0)
             except Exception as e:
                 fal_tick.append(tick)
-        if os.path.exists(path_out):
-            # Remove the directory or file
-            shutil.rmtree(path_out)
-            print(f"{path_out} has been removed.")
+        clean_backup_data(path_out, clean=True)
         table = pa.Table.from_pandas(data)
         pq.write_to_dataset(table, path_out)
-    return data
+    return data, tickers
 
 
 def read_intra_data(path_ticker, path, dictionary, update=False):
-    vietnam_time = datetime.now(pytz.timezone('Asia/Ho_Chi_Minh')).time()
-    target_time = datetime.strptime('16:00:00', '%H:%M:%S').time()
-    if vietnam_time > target_time and update:
+    """_summary_
+    Get data set of transaction if it's pass 4pm and not in weekends, update the whole dataset when set update=True
+    Args:
+        path_ticker (_type_): path of ticker parquet
+        path (_type_): path of intraday data
+        dictionary (_type_): column name
+        update (bool, optional): need update or not . Defaults to False.
+    Returns:
+        _type_: pd.DataFrame
+    """
+    vietnam_time = datetime.now(
+        pytz.timezone("Asia/Ho_Chi_Minh")
+    ).time()
+    target_time = datetime.strptime("16:00:00", "%H:%M:%S").time()
+    date0 = datetime.today().date()
+    if (
+        (vietnam_time > target_time)
+        and (update)
+        and date0.weekday() not in [5, 6]
+    ):
+
         update_intra_data(path_ticker, path, dictionary)
     data = pd.read_parquet(path)
     return data
 
 
 def update_intra_data(path_ticker, path, dictionary):
+    """_summary_
+    update intrad day data for all tickers
+    Args:
+        path_ticker (_type_): path of ticker parquet storage
+        path (_type_): path of instraday storage
+        dictionary (_type_): column name
+    """
     tickers = get_ticker(path_ticker, dictionary)
     fal_tick = []
+    data = pd.DataFrame()
     for tick, ex in zip(tickers.symbol, tickers.exchange):
         try:
             temp = read_1_ticker_intra(tick, dictionary)
@@ -225,8 +398,70 @@ def update_intra_data(path_ticker, path, dictionary):
             data = pd.concat([data, temp], axis=0)
         except Exception as e:
             fal_tick.append(tick)
+    clean_backup_data(path)
     table = pa.Table.from_pandas(data)
     pq.write_to_dataset(table, path)
 
+
+def clean_daily_data(
+    path_stock_data, path_ticker, dictionary
+) -> pd.DataFrame:
+    """_summary_
+    Data cleaning for daily stock data
+    Args:
+        data_in (_type_): list contain 2 dataframe: stock data and stock info
+        dictionary (_type_): dictionary for some columns's name that might change due to vnstock
+    Returns:
+        _type_: _description_
+    """
+    stock_data, stock_info = get_data(
+        path_stock_data, path_ticker, dictionary
+    )
+    stock_info = stock_info[stock_info["total_outstanding"] > 0]
+    stock_data = stock_data[
+        stock_data["ticker"].isin(stock_info[dictionary["ticker"]])
+    ]
+    stock_data = stock_data[
+        stock_data["ticker"].isin(stock_info[dictionary["ticker"]])
+    ]
+    stock_data = stock_data.merge(
+        stock_info[[dictionary["ticker"], "total_outstanding"]],
+        how="inner",
+        left_on=["ticker"],
+        right_on=[dictionary["ticker"]],
+    )
+    stock_data.loc[stock_data["close"] == 0, "close"] = None
+    stock_data["close"] = stock_data.groupby("ticker")[
+        "close"
+    ].fillna(method="ffill")
+    stock_data = stock_data.dropna(subset="close")
+    stock_data["market_value"] = (
+        stock_data["close"] * stock_data["total_outstanding"]
+    )
+    stock_data["return"] = (
+        stock_data["close"]
+        / stock_data.groupby("ticker")["close"].shift(1)
+        - 1
+    )
+    stock_data["log_return"] = np.log(
+        stock_data["close"]
+        / stock_data.groupby("ticker")["close"].shift(1)
+    )
+    stock_data.sort_values(["ticker", "time"], inplace=True)
+
+    stock_data["market_weight"] = stock_data[
+        "market_value"
+    ] / stock_data.groupby("exchange")["market_value"].transform(
+        "sum"
+    )
+    stock_data["return_weighted"] = (
+        stock_data["market_weight"] * stock_data["return"]
+    ).fillna(0)
+    stock_data["log_return_weighted"] = (
+        stock_data["market_weight"] * stock_data["log_return"]
+    ).fillna(0)
+    return stock_data
+
+
 if __name__ == "__main__":
-    get_data()
+    update_intra_data()
---
+++ static.py
@@ -0,0 +1,6 @@
+from function import *
+
+path_ticker = "./data/ticker"
+path_stock_data = "./data/stock"
+path_dictionary = "./name_dict.json"
+path_transaction = './data/intra/'
